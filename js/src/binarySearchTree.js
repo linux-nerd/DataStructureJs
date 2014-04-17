7f754097320a65aed150c131a9f77624c719518b
Abhishek <abhishekabby@gmail.com>
2014-04-17 14:23:27 +0530

    Modified the add node method in binarySearchTree.js

--- js/src/binarySearchTree.js
+++ js/src/binarySearchTree.js
@@ -6,50 +6,50 @@
 //==================================================================================================
 //Implementation of binary serach tree in javascript
 //==================================================================================================
-BUNNY.DS.binarySearchTree = function(){
-	
+BUNNY.DS.binarySearchTree = function() {
+
 	//private variables for node, length and root
 	var node, len = 0, root, util = {};
-	
+
 	//helper method to chech is the BST is empty
-	util.isEmpty = function(){
-		if(len == 0){
+	util.isEmpty = function() {
+		if (len == 0) {
 			return true;
-		}else{
-			return false; 
+		} else {
+			return false;
 		}
 	};
-	
+
 	//helper method to get the size of the BST
-	util.size = function(){
+	util.size = function() {
 		return len;
 	};
-	
+
 	//helper method to get the node of the BST
-	util.getNode = function(){
+	util.getNode = function() {
 		return node;
 	};
-	
+
 	//helper method to find if the searched value is there in the BST
-	util.contains = function(o){
-		try{
+	util.contains = function(o) {
+		try {
 			var returnObj = {};
-			
-			if(!util.isEmpty()){
+
+			if (util.isEmpty()) {
 				throw new Error("The BST is empty.");
-			}else{
+			} else {
 				var found = false, current = root, parentNode = null;
-				
-				while(!found && current){
-					if(o < current.data){
+
+				while (!found && current) {
+					if (o < current.data) {
 						//go left
 						parentNode = current;
 						current = current.left;
-					}else if(o > current.data){
+					} else if (o > current.data) {
 						//go right
 						parentNode = current;
 						current = current.right;
-					}else{
+					} else {
 						returnObj.result = true;
 						returnObj.currentNode = current;
 						returnObj.parentNode = parentNode;
@@ -57,132 +57,128 @@ BUNNY.DS.binarySearchTree = function(){
 					}
 				}
 			}
-		}catch(e){
+		} catch(e) {
 			returnObj.name = e.name;
 			returnObj.message = e.message;
 			returnObj.result = false;
-		}finally{
+		} finally {
 			return returnObj;
 		}
 	}
-	
-	
-	
 	// add the data to the BST
-	function add(o){
-		
-		try{
-			var createNode = BUNNY.DS.util.makeBSTNode(),
-				current;
-			
+	function add(o) {
+
+		try {
+			var createNode = BUNNY.DS.util.makeBSTNode(), current;
+
 			//add o to the node data.
 			createNode.data = o;
-			
+
 			//if the BST is empty then assign the root to the node;
-			if(util.isEmpty()){
+			if (util.isEmpty()) {
 				root = createNode;
-			}else if(util.contains(o).result){
-				throw new Error("The value is already added in the BST.");
-			}else{
-				current = node;
+				//if the tree is empty then add the node as the root and increase the length of the BST.
+				len++;
 				
+				//update the node
+				node = root;
+			} else if (util.contains(o).result) {
+				throw new Error("The value is already added in the BST.");
+			} else {
+				current = node;
+
 				//this goes into infinite loop as 1 is always true
-				while(1){
-					//if the new value is less than the node value 
-					if(o < current.data){
+				while (1) {
+					//if the new value is less than the node value
+					if (o < current.data) {
 						//if left is null
-						if(current.left === null){
+						if (current.left === null) {
 							current.left = createNode;
 							len++;
 							break;
-						}else{
+						} else {
 							current = current.left;
 						}
-					}else if(o > current.data){
-						if(current.right === null){
+					} else if (o > current.data) {
+						if (current.right === null) {
 							current.right = createNode;
 							len++;
 							break;
-						}else{
+						} else {
 							current = current.right;
 						}
-					}else{
+					} else {
 						break;
 					}
 				}
-			}catch(e){
-				return e.name + ": " + e.message; 
 			}
+		} catch(e) {
+			return e.name + ": " + e.message;
 		}
 	}
-	
-	
+
 	//remove the element from the BST
-	function remove(o){
-		try{
+	function remove(o) {
+		try {
 			//first check if the element is there in the BST
 			var isThere = util.contains(o);
-			
+
 			//check if the BST is empty
-			if(util.isEmpty()){
+			if (util.isEmpty()) {
 				throw new Error("The BST is empty.");
 			}
-			
+
 			//check if the element is there in the BST.
-			if(isThere.result){
-				// TODO: 
-				
-				if(isThere.currentNode.left == null && isThere.currentNode.right == null){
+			if (isThere.result) {
+				// TODO:
+
+				if (isThere.currentNode.left == null && isThere.currentNode.right == null) {
 					//1. is a leaf
 					//simply remove the leaf node
-					if(o < isThere.parentNode){
+					if (o < isThere.parentNode) {
 						isThere.parentNode.left = null;
-					}else if(o > isThere.parentNode){
+					} else if (o > isThere.parentNode) {
 						isThere.parentNode.right = null;
 					}
-					
 					delete isThere.currentNode
 					len--;
-					
+
 					return true;
-					
-				}else if(isThere.currentNode.left != null && isThere.currentNode.right != null){
+
+				} else if (isThere.currentNode.left != null && isThere.currentNode.right != null) {
 					//2. has two children.
-				}else if(isThere.parentNode == null){
+				} else if (isThere.parentNode == null) {
 					//3. is the root node.
-				}else{
+				} else {
 					//4. has only one child;
 				}
-				
-				
-				
-			}else{
+
+			} else {
 				throw new Error("Item not found in the BST.");
 			}
-		}catch(e){
-			return e.name + ": " + e.message; 
+		} catch(e) {
+			return e.name + ": " + e.message;
 		}
-		
+
 	}
-	
+
 	//calculate height of the tree
-	function getHeight(){
-		
+	function getHeight() {
+
 	}
-	
+
 	//calculate depth of the node
-	function getDepth(o){
-		
+	function getDepth(o) {
+
 	}
-	
-	
-	return{
-		isEmpty: util.isEmpty,
-		size: util.size,
-		getNode: util.getNode,
-		add: add,
-		remove: remove,
-		height: getHeight,
-		depth: getDepth
+
+	return {
+		isEmpty : util.isEmpty,
+		size : util.size,
+		getNode : util.getNode,
+		add : add,
+		remove : remove,
+		height : getHeight,
+		depth : getDepth
 	};
-};
+}; 
